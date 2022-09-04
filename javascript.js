790922012553b3a1a2b11aae2455aa70a9ab45de
Rajnoor-Brar <34413976+Rajnoor-Brar@users.noreply.github.com>
2022-09-04 07:45:05 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+
+var master = document.getElementsByTagName("HTML")[0]
+var vh=document.getElementById('header').clientheight
+
+function welcome(){
+
+    //master.style.scrollbehavior="initial";
+    window.scrollTo(0,0);
+
+    function setCookie(cname, cvalue, exdays) {
+      const d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      let expires = "expires="+d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+      let name = cname + "=";
+      let ca = document.cookie.split(';');
+      for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+    //master.style.scrollbehavior="smooth";
+    //window.scrollTo(0,vh);
+}
